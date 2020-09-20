8dae504f6b531e4be717ac317696073371f0b2b2
Chan <50936952+JasonChanJY@users.noreply.github.com>
2020-09-20 12:10:27 +0800

    Add files via upload

--- practice_find_contours.py
+++ practice_find_contours.py
@@ -28,14 +28,17 @@ import numpy as np
 ###线检测，圆检测
 img = cv2.imread(r'D:\opencv_lib\image\thunder.jpg', 0)
 img = cv2.resize(img, (300, 300))
-edge = cv2.Canny(img, 50, 120)
-minlength = 50
-maxLineGap = 10
-lines = cv2.HoughLinesP(edge, 1, np.pi/180, 50, minlength, maxLineGap)
+# img = np.zeros((200, 200), dtype=np.uint8)
+# img[50:150, 50:150] = 255
+edge = cv2.Canny(img, 50, 100)
+minlength = 10
+maxLineGap = 1              
+lines = cv2.HoughLinesP(edge, 1, np.pi/180, 10, minlength, maxLineGap)
+print(len(lines))
 color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-
-for x1, y1, x2, y2 in lines[0]:
-    cv2.line(color, (x1, y1), (x2, y2), (255, 0, 0), 2)
+for i in np.linspace(0, 101, 102):
+    for x1, y1, x2, y2 in lines[i]:
+        cv2.line(color, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
 cv2.imshow("edge", edge)
 cv2.imshow("lines", color)
