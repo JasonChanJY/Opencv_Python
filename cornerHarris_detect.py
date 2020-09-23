619720f6a7c6682ef3eb8a8d615a4d048198fcc9
Chan <50936952+JasonChanJY@users.noreply.github.com>
2020-09-23 14:21:21 +0800

    Update cornerHarris_detect.py

--- cornerHarris_detect.py
+++ cornerHarris_detect.py
@@ -108,7 +108,7 @@ from matplotlib import pyplot as plt
 
 
 #     if len(good) > min_match_count:
-#         src_pts = np.float32([kp1[m.gueryIdx].pt for m in good]).reshape(-1, 1, 2)
+#         src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
 #         dst_pts = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
 #         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
