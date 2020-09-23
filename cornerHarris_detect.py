b096a5df7fd0730b27f526c0d1eea7bb2e169872
Chan <50936952+JasonChanJY@users.noreply.github.com>
2020-09-23 12:05:37 +0800

    Update cornerHarris_detect.py

--- cornerHarris_detect.py
+++ cornerHarris_detect.py
@@ -45,13 +45,87 @@ from matplotlib import pyplot as plt
 
 
 # ORB特征匹配
-img1 = cv2.imread(r"D:\Pycharm_project\OpencvLib\Image\abbey_road1.jpg", 0)
-img2 = cv2.imread(r"D:\Pycharm_project\OpencvLib\Image\abbey_road.jpg", 0)
-orb = cv2.ORB_create()
-kp1, des1 = orb.detectAndCompute(img1, None)
-kp2, des2 = orb.detectAndCompute(img2, None)
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.knnMatch(des1, des2, k=2)
-# matches = sorted(matches, key=lambda x: x.distance)
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches[:50], img2, flags=2)
-plt.imshow(img3), plt.show()
+# img1 = cv2.imread(r"D:\Pycharm_project\OpencvLib\Image\abbey_road1.jpg", 0)
+# img2 = cv2.imread(r"D:\Pycharm_project\OpencvLib\Image\abbey_road.jpg", 0)
+# orb = cv2.ORB_create()
+# kp1, des1 = orb.detectAndCompute(img1, None)
+# kp2, des2 = orb.detectAndCompute(img2, None)
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+# matches = bf.knnMatch(des1, des2, k=2)
+# # matches = sorted(matches, key=lambda x: x.distance)
+# img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches[:50], img2, flags=2)
+# plt.imshow(img3), plt.show()
+
+
+# flnn匹配
+# img1 = cv2.imread(r"D:\opencv_lib\image\abbeyroad1.jpg", 0)
+# img2 = cv2.imread(r"D:\opencv_lib\image\abbeyroad.jpg", 0)
+# img1 = cv2.resize(img1, (200, 200))
+# sift = cv2.xfeatures2d.SIFT_create()
+# kp1, des1 = sift.detectAndCompute(img1, None)
+# kp2, des2 = sift.detectAndCompute(img2, None)
+
+# FLANN_INDEX_KDTREE = 0
+# indexparams = dict(algorithm= FLANN_INDEX_KDTREE, trees = 5)
+# searchparams = dict(checks = 50)
+
+# flann = cv2.FlannBasedMatcher(indexparams, searchparams)
+# matches = flann.knnMatch(des1, des1, k = 2)
+# matchMask = [[0, 0] for i in range(len(matches))]
+
+# for i, (m, n) in enumerate(matches):
+#     if m.distance < 0.7*n.distance:
+#         matchMask[i] = [1, 0]
+
+# drawparams = dict(matchColor = (0, 255, 0), singlePointColor = (255, 0, 0),
+#                   matchesMask = matchMask, flags = 0)
+# resultimage = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **drawparams)
+
+# # matches = sorted(matches, key=lambda x: x.distance)
+# plt.imshow(resultimage), plt.show()
+
+
+# flann单应性
+# img1 = cv2.imread(r"D:\opencv_lib\image\abbeyroad1.jpg", 0)
+# img2 = cv2.imread(r"D:\opencv_lib\image\abbeyroad.jpg", 0)
+# # img1 = cv2.resize(img1, (200, 200))
+# sift = cv2.xfeatures2d.SIFT_create()
+# kp1, des1 = sift.detectAndCompute(img1, None)
+# kp2, des2 = sift.detectAndCompute(img2, None)
+
+# FLANN_INDEX_KDTREE = 0
+# indexparams = dict(algorithm= FLANN_INDEX_KDTREE, trees = 5)
+# searchparams = dict(checks = 50)
+
+# flann = cv2.FlannBasedMatcher(indexparams, searchparams)
+# matches = flann.knnMatch(des1, des1, k = 2)
+
+# min_match_count = 5
+# good = []
+# for m, n in matches:
+#     if m.distance < 0.7*n.distance:
+#         good.append(m)
+
+
+#     if len(good) > min_match_count:
+#         src_pts = np.float32([kp1[m.gueryIdx].pt for m in good]).reshape(-1, 1, 2)
+#         dst_pts = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+#         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+#         matchMask = mask.ravel().tolist()
+
+#         h, w = img1.shape
+#         pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+#         dst = cv2.perspectiveTransform(pts, M)
+#         img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+
+#     else:
+#         print("not enough matches are found - %d/%d", (len(good), min_match_count))
+#         matchMask = None
+
+# drawparams = dict(matchColor = (0, 255, 0), singlePointColor = (255, 0, 0),
+#                   matchesMask = matchMask, flags = 0)
+# resultimage = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **drawparams)
+
+# # matches = sorted(matches, key=lambda x: x.distance)
+# plt.imshow(resultimage, "gray"), plt.show()
