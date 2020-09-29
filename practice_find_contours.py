c5dff3e807a361071c0c3a97fe2aa9cf8b657c46
Chan <50936952+JasonChanJY@users.noreply.github.com>
2020-09-29 15:05:10 +0800

    Update practice_find_contours.py

--- practice_find_contours.py
+++ practice_find_contours.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-###轮廓框
+
+# 轮廓框
 # img = cv2.imread(r'D:\opencv_lib\image\hammer.png', 0)
 # ret, thresh = cv2.threshold(img, 127, 255, 0)
 # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -25,25 +26,43 @@ import numpy as np
 # cv2.waitKey()
 # cv2.destroyAllWindows()
 
-###线检测，圆检测
-img = cv2.imread(r'D:\opencv_lib\image\thunder.jpg', 0)
-img = cv2.resize(img, (300, 300))
-# img = np.zeros((200, 200), dtype=np.uint8)
-# img[50:150, 50:150] = 255
-edge = cv2.Canny(img, 50, 100)
-minlength = 10
-maxLineGap = 1              
-lines = cv2.HoughLinesP(edge, 1, np.pi/180, 10, minlength, maxLineGap)
-print(len(lines))
-color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-for i in np.linspace(0, 101, 102):
-    for x1, y1, x2, y2 in lines[i]:
-        cv2.line(color, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-cv2.imshow("edge", edge)
-cv2.imshow("lines", color)
-cv2.waitKey()
-cv2.destroyAllWindows()
- 
+# 线检测
+# img = cv2.imread(r'D:\opencv_lib\image\thunder.jpg', 0)
+# img = cv2.resize(img, (300, 300))
+# # img = np.zeros((200, 200), dtype=np.uint8)  #用方块测试
+# # img[50:150, 50:150] = 255
+# edge = cv2.Canny(img, 100, 200)
+# minlength = 5
+# maxLineGap = 5
+# lines = cv2.HoughLinesP(edge, 1, np.pi/180, 10, minlength, maxLineGap)
+# color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+# for line in lines:
+#     x1, y1, x2, y2 = line[0]
+#     cv2.line(color, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+# cv2.imshow("edge", edge)
+# cv2.imshow("lines", color)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+# 圆检测
+# coins = cv2.imread("D:\opencv_lib\image\coins.jpg")
+# gray_coins = cv2.cvtColor(coins, cv2.COLOR_BGR2GRAY)
+# gray_coins = cv2.medianBlur(gray_coins, 5)
+# circles = cv2.HoughCircles(gray_coins, cv2.HOUGH_GRADIENT, 1, 120,
+#                            param1=100, param2=70, minRadius=0, maxRadius=0,)
+# circles = np.uint16(np.around(circles))
+#
+# for i in circles[0, :]:
+#     cv2.circle(coins, (i[0], i[1]), i[2], (255, 0, 0), 2)
+#     cv2.circle(coins, (i[0], i[1]), 2, (0, 255, 255), 2)
+#
+# cv2.imshow("coins", coins)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
 
 
