d204b0628c2b532eb17ebad9e191e1f3796a0364
Chan <50936952+JasonChanJY@users.noreply.github.com>
2020-09-30 21:16:43 +0800

    Update threshold_segement.py

--- threshold_segement.py
+++ threshold_segement.py
@@ -19,33 +19,33 @@ from matplotlib import pyplot as plt
 # plt.show()
 
 ###分水岭算法
-coins = cv2.imread("D:\opencv_lib\image\coins.jpg")
+coins = cv2.imread("D:/opencv_lib/image/coins1.jpg")
 gray = cv2.cvtColor(coins, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 3)
+ret_1, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_TRIANGLE + cv2.THRESH_BINARY)
+plt.subplot(321), plt.imshow(thresh), plt.title("threshold")
 
-ret_1, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU+cv2.THRESH_BINARY_INV)
-plt.subplot(321)
-plt.imshow(thresh)
-kernel = np.ones((3, 3), np.uint8)
-opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-sure_bg = cv2.dilate(opening, kernel, iterations=3)
-plt.subplot(322)
-plt.imshow(sure_bg)
-dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-ret_2, sure_fg = cv2.threshold(dist_transform, 0.06*dist_transform.max(), 255, 0)
-sure_fg = np.uint8(sure_fg) 
-plt.subplot(323)
-plt.imshow(sure_fg)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=5)
+sure_bg = cv2.dilate(opening, kernel, iterations=2)
+plt.subplot(322), plt.imshow(sure_bg), plt.title("background")  # 蓝色区域为背景
+
+dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)  # cv2.DIST_L2表示欧几里得距离， cv2.DIST_L1表示棋盘格距离， 5为掩模大小
+ret_2, sure_fg = cv2.threshold(dist_transform, 0.6*dist_transform.max(), 255, cv2.THRESH_BINARY)
+sure_fg = np.uint8(sure_fg)
+plt.subplot(323), plt.imshow(sure_fg), plt.title("frontground")  # 黄色区域为前景
+
 unknown = np.subtract(sure_bg, sure_fg)
-plt.subplot(324)
-plt.imshow(unknown)
-ret_3, markers = cv2.connectedComponents(sure_fg)
-markers = markers + 1
-markers[unknown==255] = 0
-markers = cv2.watershed(coins, markers)
-coins[markers==-1] = [0, 255, 0]
+plt.subplot(324), plt.imshow(unknown), plt.title("unknown area")
 
-plt.subplot(325)
-plt.imshow(coins)
+ret_3, markers = cv2.connectedComponents(sure_fg)  # markers 表示不同的连通区域，用不同颜色表示。如有两个连通区域，
+markers = markers + 1                              # 则markers中有三种值，分别为0、1、2，其中0表示背景
+markers[unknown==255] = 0
+plt.subplot(325), plt.imshow(markers), plt.title("markers")
+markers = cv2.watershed(coins, markers)  # markers=-1 表示边界，markers=1 表示背景（不为0的原因应该是由于前面对markers进行了加1操作），
+coins[markers==-1] = [0, 255, 0]         # markers=2 表示第一个硬币，markers=3 表示第二个硬币。。。
+
 plt.show()
+cv2.imshow("dist", coins)
 cv2.waitKey()
 cv2.destroyAllWindows()
