601cad776bfbb109c79b4fbb6a31de1249c17f25
Chan <50936952+JasonChanJY@users.noreply.github.com>
2020-10-12 17:01:56 +0800

    Update template_match.py

--- template_match.py
+++ template_match.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-targe = cv2.imread(r"D:\opencv_lib\image\abbeyroad1.jpg")
+target = cv2.imread(r"D:\opencv_lib\image\abbeyroad1.jpg")
 template = cv2.imread(r"D:\opencv_lib\image\template.png")
 
 w, h = template.shape[:2]
 #方法可以选cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF_NORMED
 #选择前两种时使用max_loc, 选择最后一种时用min_loc
-result = cv2.matchTemplate(targe, template, cv2.TM_CCORR_NORMED)
+result = cv2.matchTemplate(target, template, cv2.TM_CCORR_NORMED)
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-cv2.rectangle(targe, (max_loc[0], max_loc[1]), (max_loc[0]+w, max_loc[1]+h), [255, 0, 0],
+cv2.rectangle(target, (max_loc[0], max_loc[1]), (max_loc[0]+w, max_loc[1]+h), [255, 0, 0],
               2)
-cv2.imshow("targe", targe)
+cv2.imshow("target", target)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
